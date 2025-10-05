205cc3f0628ba691cf548b63c57c9b78e63b7761
Abhishek Kaswan <abhishekkaswan@Abhisheks-Laptop.local>
2025-10-05 20:17:46 +0530

    seperate into smaller components

--- REACT_learning/react_course/ecommerce-project/src/App.jsx
+++ REACT_learning/react_course/ecommerce-project/src/App.jsx
@@ -1,9 +1,9 @@
 import {useEffect,useState} from 'react';
 import axios from 'axios';
 import { Route, Routes } from 'react-router';
-import { HomePage } from './pages/HomePage';
+import { HomePage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import './App.css'
---
+++ REACT_learning/react_course/ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import { Header } from "../../components/Header";
+import "./HomePage.css";
+import { ProductsGrid } from './ProductsGrid';
+export function HomePage({cart}) {
+  // to fetch data from backend - we need to use useState to store the data
+  const [products, setProducts] = useState([]);
+  // need to use useEffect to fetch data from backend - so that it only runs once when the component mounts
+  useEffect(() => {
+    axios.get('/api/products')
+      .then((response)=>{
+      setProducts(response.data);
+    });
+  },[]);
+  
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <link rel="icon" type="image/svg+xml" href="images/home-favicon.png" />
+
+      <Header cart={cart} />
+
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
