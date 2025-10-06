e913e21cc813a5bba63ddb990f13a2f96fdef7d2
Abhishek Kaswan <abhishekkaswan@Abhisheks-Laptop.local>
2025-10-07 02:34:24 +0530

    test clicking add to cart button

--- REACT_learning/react_course/ecommerce-project/src/pages/home/Product.test.jsx
+++ REACT_learning/react_course/ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,7 +1,10 @@
 import {it, expect, describe, vi} from 'vitest';
 import {render, screen} from '@testing-library/react';
 import {Product} from './Product';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 
+vi.mock('axios');
 // so this render from testing libraray will render our component in a virtual dom environment and then we can use screen to query the dom and check if the elements are present or not
 describe('Product component',()=>{
     it('displays the product details correctly',()=>{
@@ -41,4 +44,38 @@ describe('Product component',()=>{
             screen.getByText("87")
         ).toBeInTheDocument();
     });
+
+    // next test user interactions like add to cart button
+    it('adds a product to the cart',async ()=>{
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+            stars: 4.5,
+            count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+        // mock function for loadCart vi.fn()=crates a fake fn that doesn't do anything
+        const loadCart = vi.fn();
+        render(<Product product={product} loadCart={loadCart} />)
+
+        // earlier we installed @testing-library/user-event to simulate user events like clicking buttons , typing in input fields etc
+        const user = userEvent.setup();
+        const addToCartButton = screen.getByTestId("add-to-cart-button");
+        await user.click(addToCartButton);
+
+        // now this will call await axios.post call for cart-items but we shouldn't call the actual api in tests so we will mock this api call
+        expect(axios.post).toHaveBeenCalledWith(
+            "/api/cart-items",
+            {
+                productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                quantity: 1
+            }
+        );
+        expect(loadCart).toHaveBeenCalled();
+    })
+
 });
